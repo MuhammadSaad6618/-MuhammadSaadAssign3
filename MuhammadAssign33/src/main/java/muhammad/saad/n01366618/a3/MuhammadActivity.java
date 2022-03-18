c5998d8fc8d0cf7cf925f11e3f573979611d995c
MuhammadSaad6618 <muhammadsaad.1274@gmail.com>
2022-03-17 22:37:05 -0400

    This is the first commit of the project.

---
+++ MuhammadAssign33/src/main/java/muhammad/saad/n01366618/a3/MuhammadActivity.java
@@ -0,0 +1,18 @@
+package muhammad.saad.n01366618.a3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//Name: Muhammad Saad
+//Student#: N01366618
+//Course: CENG258
+//Section: RNA
+
+public class MuhammadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
